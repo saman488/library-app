10e5590abc7e0cb83d2ed29a598d6d7c38ddc6bd
saman <saman1817z@gmail.com>
2024-02-19 21:45:49 +0000

    Create Home Page- Heros Componets

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
 import { Carousel } from './layouts/HomePage/Carousel';
+import { Heros } from './layouts/HomePage/Heros';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <ExploreTopBooks />
       <Carousel />
+      <Heros/>
     </div>
   );
 

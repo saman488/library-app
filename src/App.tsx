7c9f8fcadb90b9750fcedba8bce012f7ecfb6e46
saman <saman1817z@gmail.com>
2024-02-19 20:32:02 +0000

    Create Home Page- Carousel Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
 
 function App() {
   return (
     <div>
-    <Navbar/>
-    <ExploreTopBooks/>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
     </div>
   );
-  
+
 }
 
 export default App;
